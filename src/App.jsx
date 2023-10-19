7ee7b545e63d9de4e3eb0c75de1827a7f08ea4b9
Qudus Abolade <mightbeq@gmail.com>
2023-10-19 11:06:25 +0000

    update name

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Qudus Abolade",
+  title: "Data Scientist & ML/AI engineer",
+  email: "tolulopeabolade73@gmail.com.com",
+  gitHub: "https://github.com/qudus4l",
+  instagram: "https://www.instagram.com/qudus4l/",
+  linkedIn: "https://www.linkedin.com/in/qudus-abolade/",
+  medium: "https://medium.com/@mightbeq",
+  twitter: "https://twitter.com/qudus4l",
 };
 
 const primaryColor = "#4E567E";
